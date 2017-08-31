172e2e5e9f9c5bf2bb08ef707a71b7bc3824f5ee
Oleg <olekro61@gmail.com>
2017-08-31 09:16:49 +0300

    corporate11

---
+++ public/pink/js/myscripts.js
@@ -0,0 +1,78 @@
+jQuery(document).ready(function($) {
+	
+	$('.commentlist li').each(function(i) {
+		
+		$(this).find('div.commentNumber').text('#' + (i + 1));
+		
+	});
+	
+	
+	
+	$('#commentform').on('click','#submit',function(e) {
+		
+		e.preventDefault();
+		
+		var comParent = $(this);
+			
+		$('.wrap_result').
+					css('color','green').
+					text('Сохранение комментария:').
+					fadeIn(500,function() {
+						
+			var data = $('#commentform').serializeArray();
+//				alert(data);
+//			prompt(data);
+//			confirm(data);
+			
+			$.ajax({
+				url:$('#commentform').attr('action'),
+				data:data,
+				headers:{'X-CSRF-TOKEN':$('meta[name = "csrf-token"]').attr('content')},
+				type:'POST',
+				dataType:'JSON',
+				success:function(html) {
+					if(html.error) {
+						$('.wrap_result')
+											.css('color','red')
+											.append('<br /><strong>Ошибка:<strong/><br />' + html.error.join('<br />'))
+											.delay(8000)
+											.fadeOut(1000);
+					}
+					else if(html.success) {
+						$('.wrap_result')
+											.append('<br /><strong>Сохранено!<strong/>')
+											.delay(2000)
+											.fadeOut(500,function() {
+															 if(html.data.parent_id > 0) {
+																 comParent.parents('div#respond').prev().after('<ul class="children">' + html.comment + '<ul/>');
+																}
+																else {        
+																	if($.contains('#comments','ol.commentlist')) {
+																		$('ol.commentlist').append(html.comment);
+																	}
+																	else {
+																		$('#respond').before('<ol class="commentlist group">' + html.comment + '<ol/>');
+																	}
+																}
+																
+													
+																$('#cancel-comment-reply-link').click();
+															 })
+					}
+					
+				},
+				error:function() {
+					$('.wrap_result').css('color','red').append('<br /><strong>Ошибка!<strong/><br />');
+					$('.wrap_result').delay(8000).fadeOut(1000,function() {
+						$('#cancel-comment-reply-link').click();
+					});
+				}
+				
+				
+			});
+			
+		});
+		
+	});
+	
+});
